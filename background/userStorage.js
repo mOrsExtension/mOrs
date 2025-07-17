5647cc52ea44a46a87a658d89e2c917f2a39fa82
mOrsExtension <mors.extension@gmail.com>
2025-07-17 14:00:31 -0700

    Feature - turn off display for TOC & annos Fixes #93

--- background/userStorage.js
+++ background/userStorage.js
@@ -2,7 +2,7 @@
 
 /**
 * @param {string} objKey */
-const promiseGetFromStorage = async objKey => {
+const getFromStorage = async objKey => {
     try {
         const storedObj = await browser.storage.sync.get(objKey)
         if (storedObj) {
@@ -12,19 +12,19 @@ const promiseGetFromStorage = async objKey => {
             infoBG(
                 `'${objKey}' : '${objStr}'`,
                 'userStorage.js',
-                'promiseGetFromStorage'
-                ) //helper.js
+                'getFromStorage'
+                )   //helper.js
                 return storedObj[objKey]
         } else {
             warnBG(
                 'Unable to retrieve stored user preference',
                 'userStorage.js',
-                'promiseGetFromStorage'
-                ) //helper.js
+                'getFromStorage'
+                )   //helper.js
             throw new Error('Unable to retrieve stored user preference')
         }
     } catch (e) {
-        warnBG(`Error: ${e}`, 'userStorage.js', 'promiseGetFromStorage') //helper.js
+        warnBG(`Error: ${e}`, 'userStorage.js', 'getFromStorage') //helper.js
         throw e
     }
 }
